b1b5efdf3aa4ee282583e04fcfcaf4b89af127f2
MarinaPaley <marina_paley@mail.ru>
2024-09-23 15:12:02 +0300

    1.2

---
+++ 1.2/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <locale.h>
+
+/**
+* @brief Рассчитывает площадь прямоугольника.
+* @param length Длина прямоугольника.
+* @param width Ширина прямоугольника.
+* @return Площадь прямоугольника.
+*/
+double GetArea(const double length, const double width);
+
+/**
+* @brief Рассчитывает периметр прямоугольника.
+* @param length Длина прямоугольника.
+* @param width Ширина прямоугольника.
+* @return Периметр прямоугольника.
+*/
+double GetPerimetr(const double length, const double width);
+
+/**
+* @brief Считывает вещественное число.
+* @return Вещественное число.
+*/
+double Input(void);
+
+int main(void)
+{
+	setlocale(LC_ALL, "Russian");
+	puts("Введите длину прямоугольника");
+	double length = Input();
+	puts("Введите ширину прямоугольника");
+	double width = Input();
+	double area = GetArea(length, width);
+	double perimetr = GetPerimetr(length, width);
+	printf_s("Площадь прямоугольника %lf", area);
+	printf_s("\nПериметр прямоугольника %lf", perimetr);
+
+	return 0;
+}
+
+double GetArea(const double length, const double width)
+{
+	return length * width;
+}
+
+double GetPerimetr(const double length, const double width)
+{
+	return (length + width) * 2;
+}
+
+double Input(void)
+{
+	double value;
+	scanf_s("%lf", &value);
+	return value;
+}
